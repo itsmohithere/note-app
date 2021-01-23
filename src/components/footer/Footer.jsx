669ba162896480e9492a2104bc1b67ba2f8594c7
Mohit Saxena <mohitsaxena@Mohits-MacBook-Pro.local>
2021-01-23 17:50:12 +0530

    Layout implementation

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<footer className="border-2 border-grey-600 col-span-12 bottom-0 h-1/5">
+      Footer
+		</footer>
+	);
+}
---
+++ src/modules/login/Login.jsx
@@ -0,0 +1,12 @@
+export default function Login() {
+
+	return (
+		<div>
+			<form>
+				<input type="text" placeholder="Enter user name"/>
+				<input type="password" placeholder="Enter password"/>
+				<button>Login</button>
+			</form>
+		</div>
+	);
+}
