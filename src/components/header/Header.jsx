aa4f86504bde60e30c19940b00366333366a68c4
shalini-gaur-dev <shalinigaurhere@gmail.com>
2021-02-05 23:30:47 +0530

    Toggle Navigation implemented

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ToggleMenu from "../toggleMenu/ToggleMenu";
-import { ForDevice } from "media-query-react";
 
 export default function Header() {
+	const [toggle, setToggle] = useState(false); //toggle state default value is false, we can change the value of toggle using setToggle.
+	function navToggle() {
+		setToggle(!toggle); //on btn click the toggle value will be changed opposite.
+	}
 
 	const classes = {
-		header: "border-2 border-red col-span-2",
+		header: "border-2 border-red col-span-2 clear-both",
 	};
 	return (
 		<header className={classes.header}>
-			<section>
-        Mohit Saxena
+			<section className="flex">
+				<div className="flex-1 flex justify-between items-center">
+					<a href="#" className="text-xl">NOTE-APP</a>
+				</div>
+				<button className="pointer-cursor ts:hidden iPads:hidden ds:hidden" onClick={navToggle}>
+					<svg className="fill-current text-gray-900"
+						xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+						<title>menu</title>
+						<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+					</svg>
+				</button>
 			</section>
-			<ForDevice deviceName={["mobile", "mobileLandscape"]}>
-				<ToggleMenu />
-			</ForDevice>
+			<ToggleMenu toggle={toggle} />
 		</header>
 	);
 }
--- src/components/toggleMenu/ToggleMenu.jsx
+++ src/components/toggleMenu/ToggleMenu.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-export default function ToggleMenu() {
+// eslint-disable-next-line react/prop-types
+export default function ToggleMenu({ toggle }) {
+	const classes = {
+		menuShow: "block md:flex md:items-center md:w-auto w-full",
+		menuHide: "hidden",
+	};
+
 	return (
-		<section>      
-			<nav>
-        ToggleMenu
+		<section>
+			<nav className={`${toggle ? classes.menuShow : classes.menuHide}`}>
+				<ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
+					<li><a className="md:p-4 py-3 px-0 block" href="#">AboutUs</a></li>
+					<li><a className="md:p-4 py-3 px-0 block" href="#">Treatments</a></li>
+					<li><a className="md:p-4 py-3 px-0 block" href="#">Blog</a></li>
+					<li><a className="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="#">Contact Us</a></li>
+				</ul>
 			</nav>
 		</section>
 	);
